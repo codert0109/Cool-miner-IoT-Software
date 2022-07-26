8955bc50b269c43cb84859592c3b650d7f99313c
codert0109 <thebluemoonsea@gmail.com>
2022-07-26 23:17:02 +0900

    remove ensureSecure.

--- trustream-v2/dapp/backend/server.js
+++ trustream-v2/dapp/backend/server.js
@@ -62,13 +62,13 @@ https
   })
 
 function ensureSecure(req, res, next) {
-  if (req.secure) {
-    // OK, continue
-    return next()
-  }
-  // handle port numbers if you need non defaults
-  // res.redirect('https://' + req.host + req.url); // express 3.x
-  res.redirect('https://' + req.hostname + req.url) // express 4.x
+  // if (req.secure) {
+  //   // OK, continue
+  //   return next()
+  // }
+  // // handle port numbers if you need non defaults
+  // // res.redirect('https://' + req.host + req.url); // express 3.x
+  // res.redirect('https://' + req.hostname + req.url) // express 4.x
 }
 
 http.createServer(app).listen(3334)
