fddaed03b987eb153f91f7499b334c5a91073a00
codert0109 <thebluemoonsea@gmail.com>
2022-08-19 23:04:12 +0900

    update NFT design.

--- trustream-v2/dapp/frontend/src/pages/miners.tsx
+++ trustream-v2/dapp/frontend/src/pages/miners.tsx
@@ -7,10 +7,11 @@ import { NetworkState } from '@/store/lib/NetworkState';
 import $ from "axios";
 import bops from "bops";
 import { recoverPersonalSignature } from "eth-sig-util";
+import Swal from 'sweetalert2';
+import NFTContractABI from '../contracts/NFT.json';
+import ContractAddress from '../contracts/contract-address.json';
 
 const { ethereum } = require('../global.js').getWindow();
-
-
 
 const useStyles = createStyles((theme) => ({
     progressBar: {
@@ -23,65 +24,87 @@ const useStyles = createStyles((theme) => ({
 export default function TableReviews() {
     const { classes, theme } = useStyles();
     const { god, lang } = useStore();
-
     const signMessage = async () => {
         console.log('global', require('../global.js'));
         const message = 'Very Message Such Wow';
         const globalAccount = (god.currentNetwork as NetworkState).account;
-        // document.getElementById("messageString").innerHTML = JSON.stringify(message);
         try {
             const from = globalAccount;
-            console.log('from : ' + from);
-            // const msg = `${message}`;
             const msg = `0x${bops.from(message, 'utf8').toString('hex')}`;
-            // document.getElementById("messageHex").innerHTML = JSON.stringify(msg);
-            console.log('msg : ' + msg);
             const sign = await ethereum.request({
                 method: 'personal_sign',
                 params: [msg, from, 'Random text'],
             });
-            console.log('sign : ' + sign);
             return sign;
             // document.getElementById("signature").innerHTML = JSON.stringify(sign);
         } catch (err) {
             console.error(err);
+            return null;
         }
     }
 
+    const hasNFT = async () => {
+        const NFTContractAddress = ContractAddress.NFT;
+        try {
+            let data = await god.currentNetwork.execContract({
+                address : NFTContractAddress,
+                abi     : NFTContractABI.abi,
+                method  : 'balanceOf',
+                params  : [god.currentNetwork.account]
+            });
+            if (data[0] > 0)
+                return true;
+            return false;
+        } catch (err) {
+            return false;
+        }
+        return false;
+    }
+
     const onSendSignature = async () => {
+        const nft_flg = await hasNFT();
+        if (nft_flg === false) {
+            Swal.fire(
+                'Error',
+                `<p>You do not have an NFT to secure your Mining Connection.</p>
+                 <p>Please obtain a mining NFT and try again.</p>
+                 <p><a href="/nft/">Buy NFT</a></p>`,
+                'warning'
+            )
+            return false;
+        }
+
         const url = `${publicConfig.DEVICE_URL}/set_signature`;
         const signature = await signMessage();
-        const wallet = god.currentNetwork.account;
-
-        // we are currently using the same value we will change it for the future.
-        const nftID = wallet;
-        verifyMessage(signature);
-        $.post(url, { signature, nftID, wallet}, {
-
-        });
+        if (signature !== null) {
+            const wallet = god.currentNetwork.account;
+            // we are currently using the same value we will change it for the future.
+            const nftID = wallet;
+            verifyMessage(signature);
+            $.post(url, { signature, nftID, wallet }, {
+            });
+        } else {
+            Swal.fire(
+                'Error!',
+                'Errors occured while creating signature',
+                'error'
+            )
+        }
     };
 
     const verifyMessage = (signature) => {
         const message = 'Very Message Such Wow';
         const globalAccount = (god.currentNetwork as NetworkState).account;
-        // const web3 = new Web3(window.ethereum);
-
         try {
             const from = globalAccount;
             const msg = `0x${bops.from(message, 'utf8').toString('hex')}`;
-
-            const recoveredAddr = recoverPersonalSignature({data: msg, sig: signature});
-            console.log('recoveredAddr : ' + recoveredAddr);
-            // document.getElementById("recoveredAddress").innerHTML = "Recovered Address: " +
-                recoveredAddr
+            const recoveredAddr = recoverPersonalSignature({ data: msg, sig: signature });
             if (recoveredAddr.toLowerCase() === from.toLowerCase()) {
                 console.log(`Successfully ecRecovered signer as ${recoveredAddr}`);
-                // document.getElementById('result').innerHTML = `Verified !`;
             } else {
                 console.log(
                     `Failed to verify signer when comparing ${recoveredAddr} to ${from}`,
                 );
-                // document.getElementById('result').innerHTML = `Not Verified :(`;
             }
         } catch (err) {
             console.error(err);
@@ -92,7 +115,7 @@ export default function TableReviews() {
         <Layout>
             <ScrollArea>
                 <Button onClick={onSendSignature} rightIcon={<Send size={18} />} sx={{ paddingRight: 12 }}>
-                    connect to Miner
+                    Secure Miner Connection
                 </Button>
             </ScrollArea>
         </Layout>
