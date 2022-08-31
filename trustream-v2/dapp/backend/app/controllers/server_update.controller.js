dca148b247f962411fa51d283ca7b26eaf1afde7
codert0109 <thebluemoonsea@gmail.com>
2022-08-31 23:10:51 +0900

    change the version value to 1.0.1

--- trustream-v2/dapp/backend/app/controllers/server_update.controller.js
+++ trustream-v2/dapp/backend/app/controllers/server_update.controller.js
@@ -6,7 +6,7 @@ const MINER_CONFIG = require('../config/miner.config');
 exports.getUpdate = (req, res) => {
     res.send({
         status: 'OK',
-        version: '1.0.4',
+        version: '1.0.1',
         download: 'https://sharepoint/elumicate1.0.4_windows_x86_64.zip',
         message: 'We have made some improvements and have released a new version of the mining software. Please download and install. Thanks for your support. Elumicate'
     })
