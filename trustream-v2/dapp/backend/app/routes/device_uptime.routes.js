abd537c19dc8ef36404fbc2e68185d5d2a0036c5
codert0109 <thebluemoonsea@gmail.com>
2022-09-02 06:00:31 +0900

    add fixuptime function.

--- trustream-v2/dapp/backend/app/routes/device_uptime.routes.js
+++ trustream-v2/dapp/backend/app/routes/device_uptime.routes.js
@@ -1,9 +1,11 @@
 module.exports = app => {
   const device_uptime = require("../controllers/device_uptime.controller.js");
+  const { check_admin_auth } = require("../midldewares");
 
   var router = require("express").Router();
   
   router.post("/getUpTime",  device_uptime.getUpTime);
+  router.post("/fixUpTime",  check_admin_auth, device_uptime.fixUpTime);
 
   app.use("/api/device_uptime", router);
 };
--- trustream-v2/dapp/frontend/src/components/Admin/Database/index.tsx
+++ trustream-v2/dapp/frontend/src/components/Admin/Database/index.tsx
@@ -11,8 +11,15 @@ const useStyles = createStyles((theme) => ({
     gridDivBtn: {
         marginTop: '16px',
     },
+    splithorz : {
+        paddingRight : 10
+    },
     loader: {
         marginRight: '10px'
+    },
+    body : {
+        display : 'flex',
+        alignItems : 'center'
     }
 }));
 
@@ -92,12 +99,93 @@ export default function () {
         );
     };
 
+
+    const [pendingUptime, isPendingUptime] = useState(false);
+
+    const onFixUptime = () => {
+        const performAction = () => {
+            auth.$().post('https://miner.elumicate.com/api/device_uptime/fixUpTime')
+                .then((data: any) => {
+                    let info: any = data.data;
+                    if (info.status == 'OK') {
+                        Swal.fire({
+                            title: 'Success',
+                            html: `<p>Fixing Uptime Success!</p><p>Solved ${info.solved} conflicts!`,
+                            icon: 'success',
+                        });
+                    } else {
+                        Swal.fire({
+                            title: 'Info',
+                            html: `<p>${info.message}</p>`,
+                            icon: 'info',
+                        });
+                    }
+                    isPendingUptime(false);
+                })
+                .catch((err) => {
+                    Swal.fire({
+                        title: 'Error',
+                        html: `<p>Errors Occured.</p>`,
+                        icon: 'error',
+                    });
+                    isPendingUptime(false);
+                });
+        };
+
+        isPendingUptime(true);
+        auth.check_auth(
+            () => {
+                performAction();
+            },
+            () => {
+                Swal.fire({
+                    title: 'Error',
+                    html: `<p>You need to login to use admin functions.</p>`,
+                    icon: 'error',
+                    showCancelButton: true
+                }).then((result) => {
+                    if (!result.isConfirmed) {
+                        isPendingUptime(false);
+                        return;
+                    }
+                    auth.login(
+                        () => {
+                            performAction();
+                        },
+                        () => {
+                            Swal.fire({
+                                title: 'Error',
+                                html: `<p>Errors Occured while login.</p>`,
+                                icon: 'error',
+                            });
+                            isPendingUptime(false);
+                        });
+                }).catch(() => {
+                    Swal.fire({
+                        title: 'Info',
+                        html: `<p>Fixing Uptime has been cancelled.</p>`,
+                        icon: 'info',
+                    });
+                    isPendingUptime(false);
+                });
+            }
+        );
+    };
+
     return (
-        <>
-            <Button className={classes.gridDivBtn} onClick={onClearConflict}>
-                {pending && <Loader size="xs" className={classes.loader} />}
-                Clear Conflict
-            </Button>
-        </>
+        <div className={classes.body}>
+            <div className={classes.splithorz}>
+                <Button className={classes.gridDivBtn} onClick={onClearConflict}>
+                    {pending && <Loader size="xs" className={classes.loader} />}
+                    Clear Conflict
+                </Button>
+            </div>
+            <div>
+                <Button className={classes.gridDivBtn} onClick={onFixUptime}>
+                    {pendingUptime && <Loader size="xs" className={classes.loader} />}
+                    Fix Uptime
+                </Button>
+            </div>
+        </div>
     )
 }
