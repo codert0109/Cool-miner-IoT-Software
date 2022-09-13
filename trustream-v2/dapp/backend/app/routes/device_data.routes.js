662c48dd3e6b8a3aa6afdc4cead5fc5663eab69b
codert0109 <thebluemoonsea@gmail.com>
2022-09-13 19:18:50 +0900

    register api to router.

--- trustream-v2/dapp/backend/app/routes/device_data.routes.js
+++ trustream-v2/dapp/backend/app/routes/device_data.routes.js
@@ -3,31 +3,11 @@ module.exports = app => {
   const { check_admin_auth } = require("../midldewares");
 
   var router = require("express").Router();
-
-  // Create a new Tutorial
-  // router.post("/", tutorials.create);
-
-  // Retrieve all Tutorials
+  
   router.get("/",               device_data.findAll);
   router.get("/isActive",       device_data.isActive);
-  router.get("/clean",         check_admin_auth,   device_data.clean);
-
-  // router.post("/getUploadCnt",  device_data.getUploadCnt);
-
-  // Retrieve all published Tutorials
-  // router.get("/published", tutorials.findAllPublished);
-
-  // Retrieve a single Tutorial with id
-  // router.get("/:id", tutorials.findOne);
-
-  // Update a Tutorial with id
-  // router.put("/:id", tutorials.update);
-
-  // Delete a Tutorial with id
-  // router.delete("/:id", tutorials.delete);
-
-  // Delete all Tutorials
-  // router.delete("/", tutorials.deleteAll);
+  router.get("/clean",          check_admin_auth,   device_data.clean);
+  router.get("/miner",          device_data.getMinerName);
 
   app.use("/api/device_status", router);
 };
