8754a1b5a509a8fb68e88c3421ce77bc486b4cea
codert0109 <thebluemoonsea@gmail.com>
2022-09-14 19:18:57 +0900

    update web3stream with configuration.

--- trustream-v2/src/projects/app/models/index.ts
+++ trustream-v2/src/projects/app/models/index.ts
@@ -4,9 +4,9 @@ import { DeviceAuthModel } from './device_auth.model'
 import { DeviceUptimeModel } from './device_uptime.model'
 import { StatusModel } from './status.model'
 import { DeviceModel } from './device.model'
-import { PROJECT } from '@config/env';
+import { DB_SCHEMA } from '@config/env';
 
-export const db = getDB(PROJECT)
+export const db = getDB(DB_SCHEMA)
 export const deviceDataRepository = db.getRepository(DeviceDataModel)
 export const deviceAuthRepository = db.getRepository(DeviceAuthModel)
 export const deviceUptimeRepository = db.getRepository(DeviceUptimeModel)
