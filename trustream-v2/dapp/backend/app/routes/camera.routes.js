74348575e15bb630534ba87e924b3b879807bb60
codert0109 <thebluemoonsea@gmail.com>
2022-10-05 16:18:57 +0900

    add admin auth middleware to camera APIs.

--- trustream-v2/dapp/backend/app/routes/camera.routes.js
+++ trustream-v2/dapp/backend/app/routes/camera.routes.js
@@ -1,9 +1,12 @@
 module.exports = (app) => {
   const camera = require('../controllers/camera.controller.js')
+  const { check_auth } = require("../midldewares");
+
   var router = require('express').Router()
-  router.post('/add', camera.onAdd)
-  router.get('/get', camera.onGet)
-  router.post('/update', camera.onUpdate)
-  router.post('/remove', camera.onRemove)
+  
+  router.post('/add', check_auth, camera.onAdd)
+  router.get('/get', check_auth, camera.onGet)
+  router.post('/update', check_auth, camera.onUpdate)
+  router.post('/remove', check_auth, camera.onRemove)
   app.use('/api/cameras/', router)
 }
