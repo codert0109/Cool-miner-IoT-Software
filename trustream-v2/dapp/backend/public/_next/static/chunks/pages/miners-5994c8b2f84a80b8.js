19164ba1a60af5919bf1947be7bc196341bffab7
codert0109 <thebluemoonsea@gmail.com>
2022-10-18 18:29:49 +0900

    export to launch on AWS.

--- trustream-v2/dapp/backend/public/_next/static/chunks/pages/miners-5994c8b2f84a80b8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[339],{49683:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/miners",function(){return u(35322)}])}},function(n){n.O(0,[583,797,739,322,774,888,179],(function(){return _=49683,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
