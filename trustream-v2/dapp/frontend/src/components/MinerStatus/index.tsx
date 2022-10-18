b0aa41e81aabc9f3933890d5a8959ff3e82a8d8e
codert0109 <thebluemoonsea@gmail.com>
2022-10-18 18:33:30 +0900

    update miner label.

--- trustream-v2/dapp/frontend/src/components/MinerStatus/index.tsx
+++ trustream-v2/dapp/frontend/src/components/MinerStatus/index.tsx
@@ -145,7 +145,7 @@ export default observer(() => {
                 }
                 {status === false && <img src="/images/status/stopped.png" className={classes.imgStyle}></img>}
                 {status === true && <img src="/images/status/working.png" className={classes.imgStyle}></img>}
-                <span>Miner Status</span>
+                <span>Active Miners</span>
             </div>
         )
     };
