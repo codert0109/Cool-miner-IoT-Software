9b637677f4499049d4bab8dec267d49c520c3b15
codert0109 <thebluemoonsea@gmail.com>
2022-11-01 17:35:24 +0900

    export to launch on main server.

---
+++ trustream-v2/dapp/backend/public/_next/static/chunks/pages/miners-b1858b083be0eb4d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[339],{49683:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/miners",function(){return u(2207)}])}},function(n){n.O(0,[451,824,26,207,774,888,179],(function(){return _=49683,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
